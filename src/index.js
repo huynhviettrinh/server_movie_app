04f158645b74c28a0e3faf8f6b3123a79f079fe2
huynhtrinh2304 <trinhpro2304@gmail.com>
2022-04-29 21:10:30 +0700

    fit port

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const port = 3000
 const route = require('./routes/routerWeb');
 const connectDB = require('./config/connectDB')
 const bodyParser = require('body-parser')
+require('dotenv').config();
 
 // app.use(morgan('combined'))
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -14,6 +14,7 @@ route(app)
 
 connectDB();
 
+let port = process.env.PORT || 6969;
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
